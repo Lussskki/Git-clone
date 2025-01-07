e6b69d76dacf7fe3121c862bcecfcff062853de6
Luka Guledani <lukaguledani123@gmail.com>
2025-01-07 15:35:39 +0400

    import route, add body-parser and protect PORT

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
+app.use(bodyParser.json())
 
-app.listen(3000, ()=> {
+// Routes
+import gitRoute from './routes/gitRoute.js'
+app.use('/api/git', gitRoute)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=> {
     console.log(`Server running on a port 3000`)
 })
