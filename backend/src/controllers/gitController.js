7e119c8b7212393f2316274e1ebaa6fd021328f6
Luka Guledani <lukaguledani123@gmail.com>
2025-01-08 18:48:24 +0400

    fix message

--- backend/src/controllers/gitController.js
+++ backend/src/controllers/gitController.js
@@ -5,7 +5,7 @@ export const initRepo = (req, res) => {
     const gitDir = path.join(process.cwd(), 'test', '.git')
 
     if (fs.existsSync(gitDir)) {
-        return res.status(400).json({ message: '.git folder already exists in src' })
+        return res.status(400).json({ message: '.git folder already exists in test' })
     }
 
     // Create .git directory structure 
@@ -14,5 +14,5 @@ export const initRepo = (req, res) => {
     fs.mkdirSync(path.join(gitDir, 'refs'))
     fs.writeFileSync(path.join(gitDir, 'HEAD'), 'ref: refs/heads/master\n')
 
-    res.status(200).json({ message: 'Initialized an empty Git-like repository in src/.git' })
+    res.status(200).json({ message: 'Initialized an empty Git-clone repository in test/.git' })
 }
